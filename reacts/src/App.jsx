0f9fa83582161048d178cae65116abf55278d1f7
uy <a.normurzaev20@gmail.com>
2026-05-03 23:31:42 +0500

    caunt yasalmoqda

--- reacts/src/App.jsx
+++ reacts/src/App.jsx
@@ -6,6 +6,10 @@ import React, { useState } from 'react';
 const App = () => {
   const [count, setCount] = useState(0);
  
+  return (
+    <div className="container">
+      <div className="card">
+        <h2 className="title">Hisoblagich</h2>
 
 export default App;
 
