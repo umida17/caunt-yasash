24c96cccfa0bc34e4ef918a5650ca5d27578bab5
uy <a.normurzaev20@gmail.com>
2026-05-03 23:32:09 +0500

    caunt

--- reacts/src/App.jsx
+++ reacts/src/App.jsx
@@ -11,6 +11,12 @@ const App = () => {
       <div className="card">
         <h2 className="title">Hisoblagich</h2>
 
+    <div className="display-section">
+          <h1 className={`count-number ${count > 0 ? 'positive' : count < 0 ? 'negative' : ''}`}>
+            {count}
+          </h1>
+        </div>
+        
 export default App;
 
  
